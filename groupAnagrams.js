6e05a45c54a0b1a3ef2bfc15e0afb32196ec9b70
fadyehabamer <fadyamer45@gmail.com>
2026-03-30 09:36:20 +0200

    refactor groupAnagrams function for improved readability; clean up commented code and enhance logging

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -1,25 +1,24 @@
-var groupAnagrams = function (strs) {
-  // Sort each string in strs and store the sorted values in a new array
-  let sortedStrs = strs.map((str) => str.split('').sort().join(''));
+// /var groupAnagrams = function (strs) {
+// Sort each string in strs and store the sorted values in a new array
+//   let sortedStrs = strs.map((str) => str.split('').sort().join(''));
 
-  // Create a map where the keys are the sorted strings and the values are arrays of the original strings
-  let map = {};
-  for (let i = 0; i < strs.length; i++) {
-    let sortedStr = sortedStrs[i];
-    if (map[sortedStr]) {
-      map[sortedStr].push(strs[i]);
-    } else {
-      map[sortedStr] = [strs[i]];
-    }
-  }
-  let result = [];
-  for (let key in map) {
-    result.push(map[key]);
-  }
+//   // Create a map where the keys are the sorted strings and the values are arrays of the original strings
+//   let map = {};
+//   for (let i = 0; i < strs.length; i++) {
+//     let sortedStr = sortedStrs[i];
+//     if (map[sortedStr]) {
+//       map[sortedStr].push(strs[i]);
+//     } else {
+//       map[sortedStr] = [strs[i]];
+//     }
+//   }
+//   let result = [];
+//   for (let key in map) {
+//     result.push(map[key]);
+//   }
 
-  return result;
-};
-
+//   return result;
+// };
 
 // var groupAnagrams = function(strs) {
 //     let map = new Map();
@@ -34,4 +33,27 @@ var groupAnagrams = function (strs) {
 //    return [...map.values()];
 // };
 
-console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
+var groupAnagrams = function (strs) {
+  let sortedStrs = [];
+  for (let str = 0; str < strs.length; str++) {
+    sortedStrs.push(strs[str].split("").sort().join(""));
+  }
+  console.log("sortedStrs is");
+  console.log(sortedStrs);
+
+  console.log("===================================");
+
+  let strMap = new Map();
+  for (let x = 0; x < sortedStrs.length; x++) {
+    if (!strMap.has(sortedStrs[x])) {
+      strMap.set(sortedStrs[x], [strs[x]]);
+    } else {
+      strMap.get(sortedStrs[x]).push(strs[x]);
+    }
+  }
+  console.log("Map is ");
+  console.log(strMap);
+  return [...strMap.values()];
+};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
